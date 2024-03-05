2dc6549247fcd5ee5df0c09ee7a127a7d64df9bc
Ahmad Syauqi <syauqi@aptika.co.id>
2024-03-05 10:12:22 +0700

    fix: indentation

--- src/Database/Migration/1705460253396-CreateProductTable.ts
+++ src/Database/Migration/1705460253396-CreateProductTable.ts
@@ -3,20 +3,20 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 class CreateProductTable1705460253396 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-			CREATE TABLE \`products\`(
-				\`id\` CHAR(36) NOT NULL PRIMARY KEY,
-				\`name\` VARCHAR(191) NOT NULL,
-				\`description\` TEXT NOT NULL,
-				\`created_at\` BIGINT NOT NULL,
-				\`updated_at\` BIGINT NULL,
-				\`deleted_at\` BIGINT NULL
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-		`);
+		CREATE TABLE \`products\`(
+			\`id\` CHAR(36) NOT NULL PRIMARY KEY,
+			\`name\` VARCHAR(191) NOT NULL,
+			\`description\` TEXT NOT NULL,
+			\`created_at\` BIGINT NOT NULL,
+			\`updated_at\` BIGINT NULL,
+			\`deleted_at\` BIGINT NULL
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+	`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-			DROP TABLE \`products\`;
+		DROP TABLE \`products\`;
     `);
   }
 }
